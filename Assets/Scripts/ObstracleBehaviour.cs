ad5583f95230ab4242bc9ee1d2ebc4810c4654e1
KristofKotrik <72147573+KristofKotrik@users.noreply.github.com>
2020-11-03 11:41:11 +0100

    Initial commit

---
+++ Assets/Scripts/ObstracleBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstracleBehaviour : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float fallSpd = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position.Set(Random.Range(-6f, 6f), 6f, 0f);
+        //rb.AddForce(Vector2.down * fallSpd);
+        rb.velocity = Vector2.down * fallSpd;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = Vector2.down * fallSpd;
+        if (transform.position.y <= -6)
+        {
+            transform.position = new Vector3(Random.Range(-6f, 6f), 6f, 0f);
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.velocity = Vector2.down * fallSpd;
+    }
+}
---
+++ Assets/Scripts/SpaceshipBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipBehaviour : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public SpriteRenderer spriteRen;
+    public Transform RightRay;
+    public Transform LeftRay;
+    float movement = 0f;
+    public float movSpeed = 3000f;
+
+    private bool isAlive = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movement = Input.GetAxisRaw("Horizontal");
+        RaycastHit2D RHit = Physics2D.Raycast(RightRay.position, RightRay.up);
+        RaycastHit2D LHit = Physics2D.Raycast(LeftRay.position, LeftRay.up);
+
+        if (RHit) { Debug.Log(RHit.transform.name); }
+        if (LHit) { Debug.Log(LHit.transform.name); }
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(movement * movSpeed * Time.fixedDeltaTime, 0f));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isAlive = false;
+        Destroy(gameObject);
+    }
+}
